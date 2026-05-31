235d31617f4fa285026f9dbece07468b2b3aacb9
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-05-30 21:08:56 -0300

    Feat: implement title and measures atributes

--- Score.java
+++ Score.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Score {
+    private String title;
+    private List<Measure> measures = new ArrayList<>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Measure> getMeasures() {
+        return measures;
+    }
+
+    public void setMeasures(List<Measure> measures) {
+        this.measures = measures;
+    }
 }
