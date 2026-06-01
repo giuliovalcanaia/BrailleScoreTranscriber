95c5b46e3718d675ac6d10ba9ba45b6358abb17b
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-01 11:47:42 -0300

    add pitch class to the project

--- src/main/java/br/com/braille/model/Pitch.java
+++ src/main/java/br/com/braille/model/Pitch.java
@@ -1,4 +1,31 @@
 package main.java.br.com.braille.model;
 
 public class Pitch {
+    private Integer alter;
+    private Integer octave;
+    private Step step;
+
+    public Integer getAlter() {
+        return alter;
+    }
+
+    public void setAlter(Integer alter) {
+        this.alter = alter;
+    }
+
+    public Integer getOctave() {
+        return octave;
+    }
+
+    public void setOctave(Integer octave) {
+        this.octave = octave;
+    }
+
+    public Step getStep() {
+        return step;
+    }
+
+    public void setStep(Step step) {
+        this.step = step;
+    }
 }
