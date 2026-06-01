7e7cb369879185741096472f6224e527daca8fc9
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-01 11:47:57 -0300

    add Note class to the project

--- src/main/java/br/com/braille/model/Note.java
+++ src/main/java/br/com/braille/model/Note.java
@@ -1,4 +1,22 @@
 package main.java.br.com.braille.model;
 
 public class Note {
+    private Double duration;
+    private Pitch pitch;
+
+    public Double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public Pitch getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(Pitch pitch) {
+        this.pitch = pitch;
+    }
 }
