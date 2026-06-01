7b0aa2dafb5c5edf0a5ab371a38dbdafa92c74af
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-06-01 11:43:55 -0300

    add class KeySignature to the project

---
+++ src/main/java/br/com/braille/model/KeySignature.java
@@ -0,0 +1,22 @@
+package main.java.br.com.braille.model;
+
+public class KeySignature {
+    private Integer fifths;
+    private Mode mode;
+
+    public Integer getFifths() {
+        return fifths;
+    }
+
+    public void setFifths(Integer fifths) {
+        this.fifths = fifths;
+    }
+
+    public Mode getMode() {
+        return mode;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
+}
---
+++ src/main/java/br/com/braille/model/Note.java
@@ -0,0 +1,4 @@
+package main.java.br.com.braille.model;
+
+public class Note {
+}
